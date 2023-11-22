fae8c90a24132fe8b217fc9101259b6d45e2ef99
Julio Bermudez <julioph0n3@gmail.com>
2023-11-21 22:24:39 -0500

    added footer, cards and rating stars components

---
+++ client/src/components/ImageHooded/ImageHooded.jsx
@@ -0,0 +1,10 @@
+const ImageHooded = ()=>{
+    return(
+        <section className="m-5">
+        <div className="container">
+            <img src="https://eurotas-demo.myshopify.com/cdn/shop/files/banner-v4-img1_d7d23fbd-20f6-45bc-b2f9-453de902c514.jpg?v=1613790783" className="img-fluid" alt="" />
+        </div>
+        </section>
+    )
+}
+export default ImageHooded
---
+++ client/src/components/StarRating/StarRating.jsx
@@ -0,0 +1,30 @@
+
+import { useState } from 'react'
+import { Rating } from 'react-simple-star-rating'
+
+export function StarRating() {
+  const [setRating] = useState(0)
+
+  // Catch Rating value
+  const handleRating = (rate) => {
+    setRating(rate)
+
+    // other logic
+  }
+  // Optinal callback functions
+  const onPointerEnter = () => console.log('Enter')
+  const onPointerLeave = () => console.log('Leave')
+  const onPointerMove = (value, index) => console.log(value, index)
+
+  return (
+    <div className='App col-7' style={{scale:"0.5"}}>
+      <Rating
+        onClick={handleRating}
+        onPointerEnter={onPointerEnter}
+        onPointerLeave={onPointerLeave}
+        onPointerMove={onPointerMove}
+        /* Available Props */
+      />
+    </div>
+  )
+}
