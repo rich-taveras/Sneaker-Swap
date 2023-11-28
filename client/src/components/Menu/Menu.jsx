9355f19197cb6da8874eeb90b3b3a0d0921eacef
rich-taveras <rtaveras@yahoo.com>
2023-11-27 22:02:31 -0500

    connected login buttom to login page

--- client/src/components/Menu/Menu.jsx
+++ client/src/components/Menu/Menu.jsx
@@ -1,18 +1,21 @@
 import { bool } from "prop-types";
 import { StyledMenu } from "./Menu.styled";
+import React from 'react';
+import { Link } from 'react-router-dom';
+
 
 const Menu = ({ open, ...props }) => {
   const isHidden = open ? true : false;
 
   return (
-    <StyledMenu open={open} aria-hidden={!isHidden} {...props}>
+    <StyledMenu open={open} aria-hidden={!open} {...props}>
       <div className="d-flex justify-content-evenly mt-5">
         <button type="button" className="btn btn-outline-secondary">
           Menu
         </button>
-        <button type="button" className="btn btn-outline-secondary">
+        <Link to="/login" className="btn btn-outline-secondary">
           Log In
-        </button>
+        </Link>
       </div>
       <ul className="list-group mt-3">
         <li className="list-group-item mb-1">Home</li>
