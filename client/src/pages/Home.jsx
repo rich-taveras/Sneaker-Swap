7f9745f4c9389a950be132887292290ca250d516
Julio Bermudez <julioph0n3@gmail.com>
2023-11-30 17:19:24 -0500

    commented the bug on the popover

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,14 +4,11 @@ import NewArrivalTxt from "../components/NewArrivalTxt/NewArrivalTxt";
 import ImageHooded from "../components/ImageHooded/ImageHooded";
 import ProductList from "../components/ProductList";
 
-import ButtonLogIn from "../components/ButtonLogIn/ButtonLogIn";
-
 const Home = () => {
   return (
     <>
       <Hero />
       <Cards />
-      <ButtonLogIn/>
       <NewArrivalTxt />
       <ProductList />
       <ImageHooded />
