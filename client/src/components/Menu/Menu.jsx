d531f9a9f68189319159e0138d2028243fdb0f18
rich-taveras <rtaveras@yahoo.com>
2023-12-24 12:50:20 -0500

    save work

--- client/src/components/Menu/Menu.jsx
+++ client/src/components/Menu/Menu.jsx
@@ -1,37 +1,34 @@
 import { bool } from "prop-types";
 import { StyledMenu } from "./Menu.styled";
-
-import React from 'react';
-import { Link } from 'react-router-dom';
-
 import CategoryMenu from "../CategoryMenu";
 import Nav from "../Nav";
- 86917435f1d7d9879226e614388a0e37ed0e0269
-
-import CategoryMenu from "../CategoryMenu";
-import Nav from "../Nav";
- d024d107f2cd385a5d01ef654d03115234b49417
 
 const Menu = ({ open, ...props }) => {
-  const isHidden = open ? true : false;
+  const [isLoginFormVisible, setLoginFormVisible] = useState(false);
+  const [isSignupFormVisible, setSignupFormVisible] = useState(false);
+
+  const toggleLoginForm = () => {
+    setLoginFormVisible(!isLoginFormVisible);
+    setSignupFormVisible(false);
+  };
+
+  const toggleSignupForm = () => {
+    setSignupFormVisible(!isSignupFormVisible);
+    setLoginFormVisible(false);
+  };
+
+  const toggleMenu = () => {
+    setLoginFormVisible(false);
+    setSignupFormVisible(false);
+  };
+
+  const handleMouseEnter = () => {
+  };
 
   return (
     <StyledMenu open={open} aria-hidden={!isHidden} {...props}>
       <div className="d-flex justify-content-evenly mt-5">
-<<<<<<< HEAD
-<<<<<<< HEAD
-        <button type="button" className="btn btn-outline-secondary">
-          Menu
-        </button>
-        <Link to="/login" className="btn btn-outline-secondary">
-          Log In
-        </Link>
-=======
         <Nav />
->>>>>>> 86917435f1d7d9879226e614388a0e37ed0e0269
-=======
-        <Nav />
->>>>>>> d024d107f2cd385a5d01ef654d03115234b49417
       </div>
       <CategoryMenu />
     </StyledMenu>
