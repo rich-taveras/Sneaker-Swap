8272dcb689a991ef80363b0eb1841d03a105e24e
rich-taveras <rtaveras@yahoo.com>
2023-12-24 12:46:51 -0500

    save work

--- client/src/components/Menu/Menu.jsx
+++ client/src/components/Menu/Menu.jsx
@@ -1,18 +1,16 @@
 import { bool } from "prop-types";
 import { StyledMenu } from "./Menu.styled";
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-=======
 import CategoryMenu from "../CategoryMenu";
 import Nav from "../Nav";
->>>>>>> 86917435f1d7d9879226e614388a0e37ed0e0269
-=======
+ 86917435f1d7d9879226e614388a0e37ed0e0269
+
 import CategoryMenu from "../CategoryMenu";
 import Nav from "../Nav";
->>>>>>> d024d107f2cd385a5d01ef654d03115234b49417
+ d024d107f2cd385a5d01ef654d03115234b49417
 
 const Menu = ({ open, ...props }) => {
   const isHidden = open ? true : false;
