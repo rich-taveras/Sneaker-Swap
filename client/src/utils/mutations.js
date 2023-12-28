d529e6766e4a5625a7a120fe7c43f93cf8815903
rich-taveras <rtaveras@yahoo.com>
2023-12-28 06:42:22 -0500

    save work

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,24 +1,8 @@
 import { gql } from '@apollo/client';
 
-<<<<<<< HEAD
-export const ADD_USER = gql`
-  mutation addUser(
-    $firstName: String!
-    $lastName: String!
-    $email: String!
-    $password: String!
-  ) {
-    addUser(
-      firstName: $firstName
-      lastName: $lastName
-      email: $email
-      password: $password
-    ) {
-=======
 export const LOGIN = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
->>>>>>> 0b6f354e6ec9a6a759b5adf7330cf41fe05afd3e
       token
       user {
         _id
@@ -27,14 +11,6 @@ export const LOGIN = gql`
   }
 `;
 
-<<<<<<< HEAD
-export const LOGIN = gql`
-  mutation login($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
-      token
-      user {
-        _id
-=======
 export const ADD_ORDER = gql`
   mutation addOrder($products: [ID]!) {
     addOrder(products: $products) {
@@ -48,27 +24,11 @@ export const ADD_ORDER = gql`
         category {
           name
         }
->>>>>>> 0b6f354e6ec9a6a759b5adf7330cf41fe05afd3e
       }
     }
   }
 `;
 
-<<<<<<< HEAD
-export const ADD_ORDER = gql`
-  mutation addOrder($products: [ID]!) {
-    addOrder(products: $products) {
-      purchaseDate
-      products {
-        _id
-        name
-        description
-        price
-        quantity
-        category {
-          name
-        }
-=======
 export const ADD_USER = gql`
   mutation addUser(
     $firstName: String!
@@ -85,7 +45,6 @@ export const ADD_USER = gql`
       token
       user {
         _id
->>>>>>> 0b6f354e6ec9a6a759b5adf7330cf41fe05afd3e
       }
     }
   }
